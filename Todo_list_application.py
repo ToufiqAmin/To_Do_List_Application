4bad0b8f061b7297f38aff609452c7edb41d219e
Toufiq Amin <toufiqamin1995@gmail.com>
2023-09-30 00:06:12 +0600

    First Commit

---
+++ Todo_list_application.py
@@ -0,0 +1,141 @@
+#importing the required modules
+import tkinter as tk    #importing the tkinter module as tk
+from tkinter import ttk #importing the ttk module from the tkinter library
+from tkinter import messagebox #importing the messagebox module from the tkinter library
+import sqlite3 as sql   #importing the sqlite3 module as sql
+
+#defining an empty list
+tasks = []
+
+#defining the function to add tasks to the list
+def add_task():
+    #getting the string from the entry field
+    task_string = task_field.get()
+    #checking whether the string is empty or not
+    if len(task_string) == 0:
+        #displaying a message box with 'Empty Field' message
+        messagebox.showinfo('Error','Field is Empty.')
+    else:
+        #adding the string to the tasks list
+        tasks.append(task_string)
+        #using the execute() method to execute a SQL statement
+        the_cursor.execute('insert into tasks values (?)',(task_string,))
+        #calling the function to update the list
+        list_update()
+        #deleting the entry in the entry field
+        task_field.delete(0,'end')
+
+#defining the function to update the list
+def list_update():
+    #calling the function to clear the list
+    clear_list()
+    #iterating through the strings in the list
+    for task in tasks:
+        #using the insert() method to insert the tasks in the list box
+        task_listbox.insert('end',task)
+
+#defining the function to delete a task from the list
+def delete_task():
+    #using the try-except method
+    try:
+        #getting the selected entry from the list box
+        the_value = task_listbox.get(task_listbox.curselection())
+        #checking if the stored value is present in the tasks list
+        if the_value in tasks:
+            #removing the task from the list
+            tasks.remove(the_value)
+            #calling the function to update the list
+            list_update()
+            #using the execute() method to execute a SQL statement
+            the_cursor.execute('delete from tasks where title = ?',(the_value,))
+    except:
+        #displaying the message box with 'No item Selected' message for an exception
+        messagebox.showinfo('Error','No Task Selected. Cannot Delete.')
+
+#function to delete all tasks from the list
+def delete_all_tasks():
+    #displaying a message box to ask user for confirmation
+    message_box = messagebox.askyesno('Delete All','Are you sure?')
+    #if the value turns to be True
+    if message_box == True:
+        while(len(tasks)!= 0):
+            #using the pop() method to pop out the elements from the list
+            tasks.pop()
+        #using the execute() method to execute a SQL statement
+        the_cursor.execute('delete from tasks')
+        #calling the function to update the list
+        list_update()
+
+#function to clear the list
+def clear_list():
+    #using the delete method to delete all entries from the list box
+    task_listbox.delete(0,'end')
+
+#function to close the application
+def close():
+    print(tasks)
+    #using the destroy() method to close the application
+    guiWindow.destroy()
+
+#function to retrieve data from the database
+def retrieve_database():
+    #using the while loop to iterate through the elements in the tasks list
+    while(len(tasks)!=0):
+        #using the pop() method to pop out the elements from the list
+        tasks.pop()
+    #iterating through the rows in the database table
+    for row in the_cursor.execute('select title from tasks'):
+        #using the append() method to insert the tittles from the table to the list
+        tasks.append(row[0])
+
+#main function
+if __name__== "__main__":
+    #creating an object of the Tk() class
+    guiWindow = tk.Tk()
+    #setting the title of the window
+    guiWindow.title("To-Do List Manager")
+    #seeting the geometry of the window
+    guiWindow.geometry("500x500+750+250")
+    #disabling the resizable option
+    guiWindow.resizable(0,0)
+    #setting the background color to #FAEBD7
+    guiWindow.configure(bg = "#FAEBD7")
+
+    #using the connect() method to connect to the database
+    the_connection = sql.connect("listOfTasks.db")
+    #creating an object of the cursor class
+    the_cursor = the_connection.cursor()
+    #using the execute() method to execute a SQL statement
+    the_cursor.execute('create table if not exists tasks (title text)')
+
+    #defining frames using the tk.Frane() widget
+    header_frame = tk.Frame(guiWindow, bg = "#FAEBD7")
+    functions_frame = tk.Frame(guiWindow,bg = "#FAEBD7")
+    listbox_frame = tk.Frame(guiWindow, bg = "#FAEBD7")
+
+    #using the pack() method to place the frames in the application
+    header_frame.pack(fill = "both")
+    functions_frame.pack(side = "left",expand = True, fill = "both")
+    listbox_frame.pack(side = "right", expand = True, fill = "both")
+
+    #defining a label using the ttk.Label() widget
+    header_label = ttk.Label(
+        header_frame,
+        text = "The To-Do List",
+        font = ("Brush Script MT","30"),
+        background = "#FAEBD7"
+        foreground = "#8B4513"
+    )
+    #using the pack() method to place the label in the application
+    header_label.pack(padx=20,pady=20)
+
+    #defining another label using the ttk.Label() widget
+    task_label = ttk.Label(
+        functions_frame,
+        text = "Enter the Task:",
+        font = ("Consolas", "11", "bold"),
+        background = "#FAEBD7",
+        foreground = "#000000"
+    )
+    #using the place() method to place the label in the application
+    task_label.place(x = 30, y = 40)
